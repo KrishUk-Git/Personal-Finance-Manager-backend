575c2fbbe97eaaa4aec0141283d12c472fc1844b
Krish <abc@gmail.com>
2025-10-06 15:01:30 +0530

    completed

--- server.js
+++ server.js
@@ -4,32 +4,25 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const app = express();
-
 connectDB();
-
 
 const allowedOrigins = [
   'https://ukpfm.netlify.app',     
-  'http://localhost:5173',       
-  'http://localhost:5174',        
+  'http://localhost:5173',        
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-    return callback(null, true);
   }
 };
 
-app.use(cors(corsOptions)); 
-
-
+app.use(cors(corsOptions));
 
 app.use(express.json({ extended: false }));
 
@@ -39,5 +32,4 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/transactions', require('./routes/transactions'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
