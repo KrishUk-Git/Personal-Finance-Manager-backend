0e1c475655b2628dad1763de1ec230d9341b4fbf
Krish <abc@gmail.com>
2025-10-15 15:20:28 +0530

    completed

--- routes/Budgets.js
+++ routes/Budgets.js
@@ -7,16 +7,12 @@ const {
   deleteBudget,
 } = require('../controllers/budgetController');
 
-// @route   GET /api/budgets
-// @desc    Get all budgets for a user for a specific month
+
 router.get('/', auth, getBudgets);
 
-// @route   POST /api/budgets
-// @desc    Add or update a budget
+
 router.post('/', auth, addBudget);
 
-// @route   DELETE /api/budgets/:id
-// @desc    Delete a budget
 router.delete('/:id', auth, deleteBudget);
 
 module.exports = router;
